b2b3503613e3ad4d6bed0505ded8e513c2e70534
KrisErdahl <>
2018-01-06 13:13:06 -0600

    test php

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>On the Fly</title>
   </head>
   <body>
-    <h1>HIiiiiiiiiiiii</h1>
+    <h1>On the Fly</h1>
+    <script type="text/javascript">
+      //recieve POST from Java app
+      var data = <?php echo $_POST[‘my_post’] ?>;
+      console.log(data);
+
+      //
+    </script>
   </body>
 </html>
